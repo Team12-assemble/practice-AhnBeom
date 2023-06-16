246c04e2751afa849da223cb7a35691a4f1dd390
mibu119 <horang296@gmail.com>
2023-06-16 15:29:13 +0900

    🌱 계산기와 랜덤 이미지

---
+++ week2/day3/실습/calculator.js
@@ -0,0 +1,13 @@
+const inputValue = document.querySelector(".inputValue");
+const btn = document.querySelector("button");
+
+const initValue = inputValue.value;
+
+function multiple() {}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(btn.value);
+});
+
+inputValue.value = result;
