7fae8949eb76652096e7acddac13db5e18df909d
mibu119 <horang296@gmail.com>
2023-06-16 11:16:12 +0900

    💡 객체 내 함수와 this 사용

--- week2/day3/3-4.js
+++ week2/day3/3-4.js
@@ -3,9 +3,14 @@ const person = {
   lastName: "Doe",
   age: 50,
   eyeColor: "blue",
+  id: 5566,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
 
 console.log(person.firstName);
 console.log(person.age);
+console.log(person.fullName());
 
-document.getElementById("person").innerHTML = person.firstName;
+document.getElementById("person").innerHTML = person.fullName();
