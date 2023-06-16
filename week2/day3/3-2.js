e43391da17d2a854226452dc3c3f82207c79fe52
mibu119 <horang296@gmail.com>
2023-06-16 10:30:45 +0900

    💡 자바스크립트 데이터 타입

---
+++ week2/day3/3-2.js
@@ -0,0 +1,20 @@
+// Numbers
+let length = 16;
+let weight = 7.5;
+
+// strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// booleans
+let x = true;
+let y = false;
+
+// Object
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object
+const date = new Date("2022-03-25");
