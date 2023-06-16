01cd9aafdc135590de20264d3678288bdaa7827f
mibu119 <horang296@gmail.com>
2023-06-16 10:46:07 +0900

    💡 자바스크립트 객체 사용하기

---
+++ week2/day3/3-4.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+console.log(person.firstName);
+console.log(person.age);
+
+document.getElementById("person").innerHTML = person.firstName;
