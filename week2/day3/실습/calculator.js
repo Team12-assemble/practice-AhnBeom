66d284f9493e17152a1889698ef36a11c8bf6f4a
mibu119 <horang296@gmail.com>
2023-06-16 16:02:25 +0900

    💡 계산기 수정

--- week2/day3/실습/calculator.js
+++ week2/day3/실습/calculator.js
@@ -1,13 +1,21 @@
 const inputValue = document.querySelector(".inputValue");
-const btn = document.querySelector("button");
-
-const initValue = inputValue.value;
+const operators = document.getElementsByClassName("operators button");
+const btn = document.querySelectorAll("button");
 
 function multiple() {}
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(btn.value);
+let result = inputValue.value;
+
+btn[0].addEventListener("click", () => {
+  inputValue.value = btn[0].value;
+});
+
+btn[0].addEventListener("click", () => {
+  inputValue.value = btn[1].value;
+});
+
+operators[0].children[0].addEventListener("click", () => {
+  result += inputValue.value;
 });
 
 inputValue.value = result;
