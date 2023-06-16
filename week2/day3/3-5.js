218c44d559024f93c1ab89a11eec166f33d6a6e5
mibu119 <horang296@gmail.com>
2023-06-16 11:43:42 +0900

    💡 문자열 메소드 사용

--- week2/day3/3-5.js
+++ week2/day3/3-5.js
@@ -10,3 +10,18 @@ document.querySelector("#part").innerHTML = part;
 let text3 = "Please visit Microsoft!";
 let newText = text3.replace("Microsoft", "W3Schools");
 document.querySelector("#new").innerHTML = newText;
+
+let text4 = "Hello World!";
+// 대문자
+let text5 = text4.toUpperCase();
+//소문자
+let text6 = text4.toLowerCase();
+// 문자 합치기
+let text7 = "Hello";
+let text8 = "World";
+let text9 = text7.concat(" ", text8);
+
+let text10 = "Please locate where 'locate' occurs!";
+let index = text10.lastIndexOf("John"); // -1
+let index2 = text10.indexOf("locate", 15); // 21
+console.log(index, index2);
