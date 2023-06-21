5a5b3e0a070da9768004b629c2370a312be21248
mibu119 <horang296@gmail.com>
2023-06-21 22:43:19 +0900

    🌱 3주차 과제 날씨 api 사용하기

---
+++ assignment/vanillaScreen/weather.js
@@ -0,0 +1,37 @@
+import config from "./apikey.js";
+const { API_KEY } = config;
+
+const temperature = document.getElementById("temp");
+const current = document.getElementById("location");
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      const temp = json.main.temp;
+      const location = json.name;
+      temperature.innerText = `${(temp - 273.15).toFixed(2)} ℃`;
+      current.innerText = `${location}`;
+    });
+}
+
+function handleGeoSuccess(pos) {
+  const latitude = pos.coords.latitude;
+  const longitude = pos.coords.longitude;
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("error!");
+}
+
+function init() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+init();
