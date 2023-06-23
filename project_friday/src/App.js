b5e97b8161ce0db52de11bb3f5391808809f95b1
mibu119 <horang296@gmail.com>
2023-06-23 13:27:53 +0900

    🌱 토글 기능 실습

--- project_friday/src/App.js
+++ project_friday/src/App.js
@@ -1,6 +1,7 @@
 // import Greeting from "components/01";
 import Counter from "components/02";
 import Practice03 from "components/03";
+import Practice04 from "components/04";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Practice03 />
       <span>this is friday!!</span>
       <Counter />
+      <Practice04 />
     </>
   );
 }
