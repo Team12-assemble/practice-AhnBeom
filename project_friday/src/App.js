8cd64c5462699f78a69998ebbcbff4e0ea71f6fd
mibu119 <horang296@gmail.com>
2023-06-23 13:14:13 +0900

    🌱 리액트 실습 - 카운터

--- project_friday/src/App.js
+++ project_friday/src/App.js
@@ -1,10 +1,12 @@
 import Greeting from "components/01";
+import Counter from "components/02";
 
 function App() {
   return (
     <>
       <Greeting />
       <span>this is friday!!</span>
+      <Counter />
     </>
   );
 }
