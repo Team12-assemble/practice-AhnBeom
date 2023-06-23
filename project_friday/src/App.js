6c22aff989eccb32204ddd4cef7d3b54941bd863
mibu119 <horang296@gmail.com>
2023-06-23 13:36:21 +0900

    🌱 리액트 실습 map으로 목록 불러오기

--- project_friday/src/App.js
+++ project_friday/src/App.js
@@ -2,6 +2,7 @@
 import Counter from "components/02";
 import Practice03 from "components/03";
 import Practice04 from "components/04";
+import TodoList from "components/05";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <span>this is friday!!</span>
       <Counter />
       <Practice04 />
+      <TodoList />
     </>
   );
 }
---
+++ project_friday/src/components/05.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HandleTodo = ({ todos, todos2 }) => {
+  return (
+    <ul>
+      {todos.map((list) => {
+        return <li>{list.text}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default function TodoList() {
+  const todos = [
+    { id: 1, text: "할 일 목록 1" },
+    { id: 2, text: "할 일 목록 2" },
+    { id: 3, text: "할 일 목록 3" },
+    { id: 4, text: "할 일 목록 4" },
+    { id: 5, text: "할 일 목록 5" },
+  ];
+  const todos2 = [
+    { id: 1, text: "할 일 목록 1" },
+    { id: 2, text: "할 일 목록 2" },
+    { id: 3, text: "할 일 목록 3" },
+    { id: 4, text: "할 일 목록 4" },
+    { id: 5, text: "할 일 목록 5" },
+  ];
+
+  return <HandleTodo todos={todos} todos2={todos2}></HandleTodo>;
+}
