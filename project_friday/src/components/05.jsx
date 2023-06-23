5dcb19efb19f7feba2deba1e0369cb44ecb9aaf4
mibu119 <horang296@gmail.com>
2023-06-23 13:38:33 +0900

    ✨ 새로운 map 하나 추가

--- project_friday/src/components/05.jsx
+++ project_friday/src/components/05.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const HandleTodo = ({ todos, todos2 }) => {
   return (
-    <ul>
-      {todos.map((list) => {
-        return <li>{list.text}</li>;
-      })}
-    </ul>
+    <>
+      <ul>
+        {todos.map((list) => {
+          return <li>{list.text}</li>;
+        })}
+      </ul>
+      <ul>
+        {todos2.map((list) => {
+          return <li>{list.text}</li>;
+        })}
+      </ul>
+    </>
   );
 };
 
@@ -19,11 +26,11 @@ export default function TodoList() {
     { id: 5, text: "할 일 목록 5" },
   ];
   const todos2 = [
-    { id: 1, text: "할 일 목록 1" },
-    { id: 2, text: "할 일 목록 2" },
-    { id: 3, text: "할 일 목록 3" },
-    { id: 4, text: "할 일 목록 4" },
-    { id: 5, text: "할 일 목록 5" },
+    { id: 1, text: "마루는강쥐 1" },
+    { id: 2, text: "마루는강쥐 2" },
+    { id: 3, text: "마루는강쥐 3" },
+    { id: 4, text: "마루는강쥐 4" },
+    { id: 5, text: "마루는강쥐 5" },
   ];
 
   return <HandleTodo todos={todos} todos2={todos2}></HandleTodo>;
