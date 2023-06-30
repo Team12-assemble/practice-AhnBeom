07c0ac5601d78695387d2e66a5ea2ea4b1c76e7d
mibu119 <horang296@gmail.com>
2023-06-30 14:56:22 +0900

    ✨ 실시간 채팅 완성

--- react-practices/src/components/practices/LiveChat.jsx
+++ react-practices/src/components/practices/LiveChat.jsx
@@ -1,35 +1,67 @@
 import io from "socket.io-client";
-import { useState } from "react";
-import useForm from "../custom-hooks/useForm";
+import { useEffect, useState } from "react";
 
+// node server.js 실행 우선
 const socket = io("http://localhost:3001");
 
 export default function LiveChat() {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
   const [chats, setChats] = useState([]);
-  const { values, handleChange } = useForm({
-    name: "",
-    message: "",
-  });
+
+  useEffect(() => {
+    socket.on("message", handleMessage);
+    return () => {
+      socket.off("message", handleMessage);
+    };
+  }, []);
+
+  const handleMessage = (message) => {
+    setChats((prev) => [...prev, message]);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setChats([...chats, values.message]);
+    if (message.trim() !== "") {
+      const currentTime = new Date().toLocaleDateString();
+      socket.emit("message", {
+        username,
+        content: message,
+        time: currentTime,
+      });
+
+      setMessage("");
+    }
   };
 
   return (
     <section>
+      <h1>실시간 채팅</h1>
+      <input
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        name="username"
+        value={username}
+        placeholder="사용자 이름을 입력해주세요."
+      />
       <ul>
-        {chats.map((message) => {
+        {chats.map((chat, i) => {
           return (
-            <li>
-              <label>{values.name}</label>: {message}
+            <li key={i}>
+              <label>{chat.username}</label>: {chat.content} - {chat.time}
             </li>
           );
         })}
       </ul>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} name="name" value={values.name} />
-        <input onChange={handleChange} name="message" value={values.message} />
+        <input
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          name="message"
+          value={message}
+        />
         <button type="submit">Enter</button>
       </form>
     </section>
