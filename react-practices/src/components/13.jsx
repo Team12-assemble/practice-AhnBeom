720ecb53ee3a590ff7a927f729f17471298acf2e
mibu119 <horang296@gmail.com>
2023-06-30 13:15:20 +0900

    🎨 useReducer를 사용하여 theme 끄고 키기

--- react-practices/src/components/13.jsx
+++ react-practices/src/components/13.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import { useReducer } from "react";
+// useReducer를 사용한 방식 1
+const initial = {
+  theme: "light",
+};
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_THEME":
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    default:
+      return state;
+  }
+};
 export default function ToggleTheme() {
-  const [isLight, setIsLight] = useState(false);
+  // useState를 사용한 방식
+  //   const [isLight, setIsLight] = useState(false);
 
-  const handleToggle = () => {
-    setIsLight((prev) => !prev);
-  };
+  //   const handleToggle = () => {
+  //     setIsLight((prev) => !prev);
+  //   };
+
+  // useReducer를 사용한 방식 2
+  const [isLight, dispatch] = useReducer(reducer, initial);
   return (
     <div>
-      <p>current theme: {isLight ? "light" : "dark"}</p>
-      <button onClick={handleToggle}>toggle theme</button>
+      <p>current theme: {isLight.theme}</p>
+      <button onClick={() => dispatch({ type: "TOGGLE_THEME" })}>
+        toggle theme
+      </button>
     </div>
+    // <div>
+    //   <p>current theme: {isLight ? "light" : "dark"}</p>
+    //   <button onClick={handleToggle}>toggle theme</button>
+    // </div>
   );
 }
