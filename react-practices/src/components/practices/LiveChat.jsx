838403071ffff3041b5a87977c36514001e2be88
mibu119 <horang296@gmail.com>
2023-06-30 13:51:15 +0900

    📦 👽️ socket io 패키지 설치

--- react-practices/src/components/practices/LiveChat.jsx
+++ react-practices/src/components/practices/LiveChat.jsx
@@ -1,5 +1,8 @@
+import io from "socket.io-client";
 import { useState } from "react";
 import useForm from "../custom-hooks/useForm";
+
+const socket = io("http://localhost:3001");
 
 export default function LiveChat() {
   const [chats, setChats] = useState([]);
